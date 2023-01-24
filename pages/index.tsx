a0ca45d97bb10d496ff67b4e1dbf29b653b956a5
Mathieu Grondin <mathieu.grondin@ubisoft.com>
2023-01-24 13:54:34 -0500

    increase font size

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.description}>Woow trop sick</h1>
+        <h1 className={styles.description}>Woow trop sick 🤘</h1>
       </main>
     </>
   )
